a5d17e6608594770f4ec4960ad23e5f0765b9126
ruanqiaohua <ruanqiaohua@hotmail.com>
2016-01-19 21:10:25 +0800

    Initial Commit

---
+++ LiveMediaPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveMediaPlayer
+//
+//  Created by ruanqiaohua on 16/1/19.
+//  Copyright © 2016年 ruanqiaohua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
